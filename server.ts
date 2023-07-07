46f852b6e0009cfb8fb731f21b893fc7ca76c72d
Braxton Goss <braxtonagoss@gmail.com>
2023-07-07 16:25:21 -0700

    authentication work pt1

--- server.ts
+++ server.ts
@@ -4,19 +4,49 @@ import { Server, Socket } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, world!");
+// Middleware to authenticate requests
+const authenticate = (req: Request, res: Response, next: () => void) => {
+  // Implement your authentication logic here
+  // For example, you can check if the request contains valid credentials or a valid session
+
+  // If authentication succeeds, call the next middleware or route handler
+  next();
+
+  // If authentication fails, you can send an error response
+  // For example:
+  // res.status(401).send("Unauthorized");
+};
+
+app.use(express.static("public"));
+
+// Apply the authentication middleware to specific routes
+app.get("/", authenticate, (req: Request, res: Response) => {
+  res.send("Hello, authenticated user!");
+});
+
+app.post("/", authenticate, (req, res) => {
+  res.send("Authenticated user, received a POST request");
+});
+
+// Socket.IO authentication
+io.use((socket: Socket, next: (err?: Error) => void) => {
+  // Implement your socket authentication logic here
+  // For example, you can check if the socket connection contains valid credentials or a valid session
+
+  // If authentication succeeds, call the next middleware
+  next();
+
+  // If authentication fails, you can close the socket connection
+  // For example:
+  // next(new Error("Unauthorized"));
 });
 
 io.on("connection", (socket: Socket) => {
   console.log("A user connected");
 
-  // Handle socket events
   socket.on("chatMessage", (message: string) => {
-    // Process and broadcast the message
     io.emit("chatMessage", message);
   });
 
@@ -25,8 +55,7 @@ io.on("connection", (socket: Socket) => {
   });
 });
 
-const port = 3000; // Choose your desired port number
-
+const port = 3000;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
